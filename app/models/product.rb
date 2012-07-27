75dc3d23efab4361f2561fb4cc510f6463fcf9bd
Loveday <loveday.anyim@student.ncirl.ie>
2012-07-27 15:36:32 +0100

    Validation Tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,16 @@ class Product < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
 
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+      with:    %r{\.(gif|jpg|png)\Z}i,
+      message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  validates :title, length: {minimum: 10}
+
   #@REFERENCE LaptopShop Tutorial 1
   def self.search(search_query)
     if search_query
