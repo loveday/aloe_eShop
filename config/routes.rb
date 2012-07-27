cfab9511730e64166f7694b912e24888f7316f0a
Loveday <loveday.anyim@student.ncirl.ie>
2012-07-27 16:26:15 +0100

    Prettier listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AloeVeraHealthStore::Application.routes.draw do
+  get "store/index"
+
   resources :line_items
 
   resources :carts
@@ -66,11 +68,12 @@ AloeVeraHealthStore::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome", :action => "home"
+  # root :to => "welcome", :action => "home"
+  root to: 'store#index', as: 'store'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+   match ':controller(/:action(/:id(.:format)))'
 end
